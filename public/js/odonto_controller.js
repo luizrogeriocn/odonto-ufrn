44f07451e46951a50ba9564d8faaccc41dca2f3e
Luiz Rogerio <luizrogeriocn@gmail.com>
2014-11-05 18:16:26 -0300

    Implements score calculation

--- public/js/odonto_controller.js
+++ public/js/odonto_controller.js
@@ -16,17 +16,19 @@ app.config( function ( $routeProvider ) {
 });
 
 app.controller("odonto_controller", function($scope){
-    $scope.question = {text: "Qual seu sexo?", alternatives: [{text: "Homem", score: 1, color: "blue"}, {text: "Mulher", score: 0, color: "red"}]};
-    $scope.question2 = {text: "Qual a sua idade?", alternatives: [{text: "Até 10 anos", score: 1, color: "blue"}, {text: "10 a 15 anos", score: 0, color: "red"}, {text: "15 a 20 anos", score: 0, color: "red"}, {text: "Mais de 20 anos", score: 0, color: "red"}]};
-    $scope.question3 = {text: "Você possui dentes a mais?", alternatives: [{text: "Sim", score: 1, color: "red"}, {text: "Não", score: 0, color: "blue"}]};
-    $scope.question4 = {text: "Você possui dentes mal posicionados?", alternatives: [{text: "Sim", score: 1, color: "red"}, {text: "Não", score: 0, color: "blue"}]};
-    $scope.questions = [$scope.question, $scope.question2, $scope.question3, $scope.question4];
+    $scope.question = {text: "Qual seu sexo?", alternatives: [{text: "Homem", score: 8, color: "blue"}, {text: "Mulher", score: 10, color: "red"}]};
+    $scope.question2 = {text: "Qual a sua idade?", alternatives: [{text: "Até 10 anos", score: 4, color: "blue"}, {text: "10 a 15 anos", score: 10, color: "red"}, {text: "15 a 20 anos", score: 8, color: "red"}, {text: "Mais de 20 anos", score: 8, color: "red"}]};
+    $scope.question3 = {text: "Você possui dentes mal posicionados?", alternatives: [{text: "Na frente", score: 6, color: "red"}, {text: "Na parte de trás", score: 8, color: "blue"}, {text: "Na parte de trás e na frente", score: 10, color: "blue"}, {text: "Não", score: 4, color: "blue"}]};
+    $scope.question4 = {text: "Você possui dentes a mais?", alternatives: [{text: "Sim", score: 10, color: "red"}, {text: "Não", score: 4, color: "blue"}]};
+    $scope.question5 = {text: "Você já perdeu algum dente?", alternatives: [{text: "Sim", score: 10, color: "red"}, {text: "Não", score: 4, color: "blue"}]};
+    $scope.questions = [$scope.question, $scope.question2, $scope.question3, $scope.question4, $scope.question5];
 
     $scope.current_question;
     $scope.current_question_id = 0;
     $scope.current_question = $scope.questions[$scope.current_question_id];
 
     $scope.choices = [];
+    $scope.current_score = 0;
 
     $scope.next_question = function(){
         if($scope.current_question_id+1 < $scope.questions.length){
@@ -51,5 +53,18 @@ app.controller("odonto_controller", function($scope){
     $scope.choose_alternative = function(question, alternative){
         $scope.choices[question] = alternative;
         console.log("Você escolheu a alternativa #"+ (alternative+1) +" na questão #"+(question+1));
+        $scope.calculate_score();
+    };
+
+    $scope.calculate_score = function(){
+        var chosen_question;
+
+        for (var i = $scope.questions.length - 1; i >= 0; i--) {
+            if($scope.choices[i] != undefined){
+                chosen_question = $scope.choices[i];
+                $scope.current_score += $scope.questions[i].alternatives[chosen_question].score;
+            }
+        };
+        console.log("Seu score atual é: "+$scope.current_score);
     };
 });
